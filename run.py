5a2034c9624dbf82e0d54b6e4f476a7a3a4d9893
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2025-03-06 15:34:42 -0500

    Another check

--- run.py
+++ run.py
@@ -50,7 +50,7 @@ def create_repo_list(start_date, end_date):
 
         page = 1
         while True:
-            repos = search_github_repos(f"lang:vbnet pushed:<{current_date}", sort="updated", order="asc", per_page=100, page=page)
+            repos = search_github_repos(f"lang:vbnet pushed:<={current_date}", sort="updated", order="asc", per_page=100, page=page)
 
             #Debugging: Write the response to a file by converting it to a json string
             # with open('repos.json', 'a') as f:
